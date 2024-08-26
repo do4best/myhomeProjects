1a3e342a1a657c1b5f83f9e410d50649aa12fcdb
Meer Afzal <do4best@gmail.com>
2024-08-26 15:31:57 +0500

    We had a little improvement in the programme using syncronizing in income & expediture app

--- incomeExpenditureApp/Balance.jsx
+++ incomeExpenditureApp/Balance.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import Header from "./Header.jsx";
+import React, {useContext} from 'react';
+import {GlobalContext} from "./containercomponents/glocalData.jsx";
 
 function Balance(props) {
+    const {Transactions} = useContext(GlobalContext)
+    const amounts = Transactions.map(transaction => transaction.amount)
+    const total = amounts.reduce((acc, cur) => ( acc += cur ), 0).toFixed(2);
     return (
         <>
-            <h4 className={"text-center mt-4"}>Your Balance <span className={"text-success"}>$0.00</span></h4>
+            <h4 className={"text-center mt-4"}>Your Balance <span className={"text-success"}>${total}</span></h4>
 
         </>
     );
--- incomeExpenditureApp/incomeExpance.jsx
+++ incomeExpenditureApp/incomeExpance.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {GlobalContext} from "./containercomponents/glocalData.jsx";
 
 function IncomeExpance(props) {
+    const {Transactions} = useContext(GlobalContext);
+    const amount = Transactions.map(transaction => transaction.amount);
+    const income = amount.filter(transaction => transaction > 0).reduce((accu,item) => (accu + item),0).toFixed(2);
+    const expanse = (amount.filter(transaction => transaction < 0).reduce((accu, item) => (accu + item),0)*-1).toFixed(2);
     return (
         <>
             <div className="inc-exp-container">
                 <div>
                     <h4>Income</h4>
-                    <p id="money-plus" className="money plus">+$0.00</p>
+                    <p id="money-plus" className="money plus">{income}</p>
                 </div>
                 <div>
                     <h4>Expense</h4>
-                    <p id="money-minus" className="money minus">-$0.00</p>
+                    <p id="money-minus" className="money minus">-{expanse}</p>
                 </div>
             </div>
 
